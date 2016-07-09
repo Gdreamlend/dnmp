5cae462dce6d59f59c0f4defae85e283a0231885
Reid Niu <reid.niu@gmail.com>
2016-07-09 12:35:39 +0800

    add some functions

--- build.sh
+++ build.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 
 hr='————————————————————————————————————————————————'
+prefix='|              '
 if [ $1 ]; then
        pname=$1
        echo $hr
-       echo "  项目名称为 $pname "
+       echo "$prefix 项目名称为 $pname "
        echo $hr
        else
        pname='web'
        echo $hr
-       echo "  没有定义项目名, 默认为 $pname "
+       echo "$prefix 没有定义项目名, 默认为 $pname "
        echo $hr
 fi
 
 echo $hr
-echo "  删除已有 $pname 容器"
+echo "$prefix 删除已有 $pname 容器"
 echo $hr
 docker rm -f $pname
 
 echo $hr
-echo "  删除已有 $pname 镜像"
+echo "$prefix 删除已有 $pname 镜像"
 echo $hr
 docker rmi -f $pname
 
 echo $hr
-echo "  创建 $pname 镜像"
+echo "$prefix 创建 $pname 镜像"
 echo $hr
 docker build -t $pname .
 
 
 if [ ! -d ../$pname/nginx ] ; then
   echo $hr
-  echo "  Nginx配置目录不存在，使用默认..."
+  echo "$prefix Nginx配置目录不存在，使用默认..."
   echo $hr
   mkdir ../$pname/nginx
   cp conf/default.conf ../$pname/nginx/default.conf
@@ -40,7 +41,7 @@ fi
 
 if [ ! -d ../$pname/www ] ; then
   echo $hr
-  echo "  www目录不存在，使用默认..."
+  echo "$prefix www目录不存在，使用默认..."
   echo $hr
   mkdir -p ../$pname/www/default
   cp conf/index.php ../$pname/www/default/
@@ -49,7 +50,7 @@ fi
 
 if [ ! -d ../$pname/logs ] ; then
   echo $hr
-  echo "  Logs目录不存在，创建..."
+  echo "$prefix Logs目录不存在，创建..."
   echo $hr
   mkdir ../$pname/logs
 fi
@@ -57,21 +58,25 @@ fi
 
 if [ ! -d ../$pname/mysql ] ; then
   echo $hr
-  echo "  Mysql目录不存，创建..."
+  echo "$prefix Mysql目录不存，创建..."
   echo $hr
   mkdir ../$pname/mysql
 fi
 
 echo $hr
-echo "  创建 $pname 容器，运行并挂载目录 "
+echo "$prefix 创建 $pname 容器，运行并挂载目录 "
 echo $hr
 
 if [ $2 ]; then
-       port=$2
-       echo "端口为 $port "
+           port=$2
+           echo $hr
+           echo "$prefix 端口为 $port "
+           echo $hr
        else
-       port=80
-       echo "没有定义端口, 默认为 $port "
+           port=80
+           echo $hr
+           echo "$prefix 没有定义端口, 默认为 $port "
+           echo $hr
 fi
 
 docker run -d \
@@ -84,16 +89,16 @@ docker run -d \
   $pname
 
 echo $hr
-echo "    显示当前的镜像列表"
+echo "$prefix 显示当前的镜像列表"
 echo $hr
 docker images
 
 echo $hr
-echo "    显示当前容器的进程"
+echo "$prefix 显示当前容器的进程"
 echo $hr
 docker ps
 
 echo $hr
-echo "    输出 $pname 容器日志"
+echo "$prefix 输出 $pname 容器日志"
 echo $hr
 docker logs $pname
